f3520a33bad3b642816e80eb788f3a52a4ad88ff
Alexander-Meldrum <alexander.meldrum.jp@gmail.com>
2023-04-17 20:06:48 +0200

    Adding first files

---
+++ csv_to_html.py
@@ -0,0 +1,20 @@
+import pandas as pd
+# Convert csv table to html table body and combine the html files.
+df = pd.read_csv('Automotive.csv')
+#print("The csv data is:")
+#print(df)
+# Replace https links with hmtml anchors <a>
+for index, row in df.iterrows():
+    #print(row['Tool'], row['Link'])
+    print(row['Link'])
+    if row['Link'] is None:
+        print("hej2")
+
+
+html_string = df.to_html(header=False, index=False, na_rep="")
+#print("The html string is:")
+#print(html_string)
+
+text_file = open("Generated.html", "w")
+text_file.write(html_string)
+text_file.close()
