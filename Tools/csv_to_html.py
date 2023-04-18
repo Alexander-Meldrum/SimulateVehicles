08f9beb1aa3168d07c7636af686bc2da903bbf4f
Alexander Meldrum <alexander.meldrum.jp@gmail.com>
2023-04-18 20:00:02 +0200

    Changes

--- Tools/csv_to_html.py
+++ Tools/csv_to_html.py
@@ -1,15 +1,18 @@
 import pandas as pd
 import numpy as np
+
 # Convert csv table to html table body and combine the html files.
+
+# Read csv file into pandas df
 df = pd.read_csv('Automotive.csv')
 #print("The csv data is:")
 #print(df)
+
 # Replace Tool name with html anchors <a> if link defined
 df['Tool'] = np.where(df['Link'].isna(), df['Tool'], '<a href="' + df['Link'] + '"' + ' target="_blank">'+ df['Tool']+ '</a>')
-print(df)
+#print(df)
 
-#TODO remove link column
-html_string = df.to_html(header=False, index=False, na_rep="")
+html_string = df.to_html(header=False, index=False, na_rep="", escape=False, columns=['Tool', 'Features','OS/Framework','Open Source', 'Description'])
 #print("The html string is:")
 #print(html_string)
 
@@ -17,7 +20,7 @@ text_file = open("generated_table_body.html", "w")
 text_file.write(html_string)
 text_file.close()
 
-#Remove first row from Generated_table_body
+# Remove first row from Generated_table_body
 with open('generated_table_body.html', 'r') as fin:
     data = fin.read().splitlines(True)
 with open('generated_table_body_modified.html', 'w') as fout:
